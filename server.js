e638ac88d577c6f91c595e2b7c36c6d6bf442388
Hamid-00 <78131168+Hamid-00@users.noreply.github.com>
2021-04-15 11:21:58 -0400

    Lab update

--- server.js
+++ server.js
@@ -1,27 +1,25 @@
-import express from 'express';
-import db from './database/initializeDB.js';
-import apiRoutes from './routes/apiRoutes.js';
-
-const app = express();
-
-const PORT = process.env.PORT || 3000;
-const staticFolder = 'public';
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static(staticFolder));
-app.use('/api', apiRoutes);
-
-async function bootServer() {
-  try {
-    const mysql = await db.sequelizeDB;
-    await mysql.sync();
-    app.listen(PORT, () => {
-      console.log(`Listening on: http//localhost:${PORT}`);
-    });
-  } catch (err) {
-    console.error(err);
-  }
+function getRandomIntInclusive(min, max) {
+  min = Math.cell(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() *( max - min + 1) + min);
 }
 
-bootServer();
+async function getMeals() {
+  console.log('data request');
+  const diningRequest = await fetch('/api/wholeMeal');
+  const diningRequest = await diningRequest.json();
+  return diningData;
+}
+async function windowActions() {
+  console.log('loaded window');
+  const results = await getMeals();
+  const meals = results.data;
+  console.table(meals);
+
+  const mealArray = [1,2,3,4,5,6,7,8,9,10];
+  const selectedMeals = mealArray.map((element) => {
+    const random = getRandomIntInclusive(0,meals.length-1);
+    return meals[random];
+  });
+}
+window.onload = windowActions;
